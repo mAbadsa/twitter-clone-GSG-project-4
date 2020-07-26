ad9b50ff2faa9ad12ac175ed0ede704805385c01
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-07-26 17:29:41 +0300

    Add renderListOfTweet and listTweet

--- src/js/models/tweet.model.js
+++ src/js/models/tweet.model.js
@@ -1,6 +1,25 @@
 export class Tweet {
-  constructor(id, text) {
+  constructor(
+    id,
+    text,
+    tweetStatus,
+    name,
+    username,
+    likes,
+    retweet,
+    comment,
+    tweeterAvatar,
+    createdAt
+  ) {
     this.id = id;
     this.text = text;
+    this.tweetStatus = tweetStatus;
+    this.name = name;
+    this.username = username;
+    this.likes = likes;
+    this.retweet = retweet;
+    this.comment = comment;
+    this.tweeterAvatar = tweeterAvatar;
+    this.createdAt = createdAt;
   }
 }
